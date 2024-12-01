5170ab29bea572958a24d465dcefd54ea187f686
htp28gthb <phat.ho1692@gmail.com>
2024-12-01 10:46:35 +0700

    Removed all projects cause it's private

--- projects/portfolios/simple_portfolio_2/js/main.js
+++
@@ -1,7 +0,0 @@
-function openRightMenu() {
-	const navbarList = document.querySelector('.navbar__list');
-	const hamburger = document.querySelector('.hamburger');
-
-	navbarList.classList.toggle('showNav');
-	hamburger.classList.toggle('toggle');
-}
